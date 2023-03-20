242e46deccccde7e07ced5d008bf6c95e076768e
Nestsiarenka Pavel Evgenyevich <PNesterenko@alfabank.ru>
2023-03-20 11:27:10 +0300

    version update up to 0.6.2-beta

--- buildSrc/src/main/kotlin/BotProjectConfigs.kt
+++ buildSrc/src/main/kotlin/BotProjectConfigs.kt
@@ -1,6 +1,6 @@
 object BotProjectConfigs {
 
     // do not forget to change version in BotConfigs.kt
-    const val VERSION = "0.6.1-beta"
+    const val VERSION = "0.6.2-beta"
     const val GROUP = "com.nesterenko"
 }
--- src/main/kotlin/core/BotConfigs.kt
+++ src/main/kotlin/core/BotConfigs.kt
@@ -2,5 +2,5 @@ package core
 
 object BotConfigs {
 
-    const val VERSION = "0.6.1-beta"
+    const val VERSION = "0.6.2-beta"
 }
