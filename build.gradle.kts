3aba4299c85c31949a1e6158c7d695c8a7ffbd81
Nestsiarenka Pavel Evgenyevich <PNesterenko@alfabank.ru>
2023-04-02 18:00:39 +0300

    replace ksp with kapt

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.0"
-    id("com.google.devtools.ksp") version "1.8.0-1.0.8"
+    kotlin("kapt") version "1.8.10"
     application
 }
 
@@ -18,11 +18,11 @@ dependencies {
     implementation("net.dv8tion:JDA:$jda")
 
     val moshi = "1.14.0"
-    ksp("com.squareup.moshi:moshi-kotlin-codegen:$moshi")
+    kapt("com.squareup.moshi:moshi-kotlin-codegen:$moshi")
 
     val moshix = "1.7.20-Beta-0.18.3"
     implementation("dev.zacsweers.moshix:moshi-sealed-runtime:$moshix")
-    ksp("dev.zacsweers.moshix:moshi-sealed-codegen:$moshix")
+    kapt("dev.zacsweers.moshix:moshi-sealed-codegen:$moshix")
 }
 
 tasks.test {
