45ca6cf0fc667269dfb3ce4608515702babb6ec5
Nestsiarenka Pavel Evgenyevich <PNesterenko@alfabank.ru>
2023-04-16 18:05:46 +0300

    update version up to 1.5-beta

--- buildSrc/src/main/kotlin/BotProjectConfigs.kt
+++ buildSrc/src/main/kotlin/BotProjectConfigs.kt
@@ -1,6 +1,6 @@
 object BotProjectConfigs {
 
     // do not forget to change version in BotConfigs.kt
-    const val VERSION = "1.3-beta"
+    const val VERSION = "1.5-beta"
     const val GROUP = "com.nesterenko"
 }
